38eac888c69715718a9f55e8864a342d31d78b00
MrGeislinger <GitHub@MrGeislinger.com>
2021-03-07 12:53:38 -0800

    1st complete run without going off track

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,8 @@ int main() {
   uWS::Hub h;
 
   PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
   // Initializing with reasonable values
-  pid.Init(-0.01 , -0.0001 , -1.5);
+  pid.Init(-0.1 , -0.0001 , -1.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -59,12 +56,8 @@ int main() {
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
-          /**
-           * TODO: Calculate steering value here, remember the steering value is
-           *   [-1, 1].
-           * NOTE: Feel free to play around with the throttle and speed.
-           *   Maybe use another PID controller to control the speed!
-           */
+          
+          /***** Calculate steering value *****/
           // Update error from CTE
           pid.UpdateError(cte);
           // Get steering using the full error term
